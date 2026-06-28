c652446597125bf3572f8950987da42b389f1926
Alejandro Cabello Jiménez <alejandro.cabello89@gmail.com>
2026-06-28 16:24:35 +0200

    chore: desktop-pet 0.18.0

--- Casks/desktop-pet.rb
+++ Casks/desktop-pet.rb
@@ -1,6 +1,6 @@
 cask "desktop-pet" do
-  version "0.17.0"
-  sha256 "1623c17f9a4c234c854de6c095fc3dcb41467112b48af376b08b129ecee49f2d"
+  version "0.18.0"
+  sha256 "b1dc2c59fb25cfe0cca540379e777d63b5e8073e859030ad74fa083f200d0a3a"
 
   url "https://github.com/acabelloj/desktop-pet-releases/releases/download/v#{version}/DesktopPet"
   name "Desktop Pet"
